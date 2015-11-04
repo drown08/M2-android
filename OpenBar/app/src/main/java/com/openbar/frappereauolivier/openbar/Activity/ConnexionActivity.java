0ed231a7cc5c1a3b13e7f01a9f50966894e244e5
Frappereau Olivier <olivier.frappereau@etu.umontpellier.fr>
2015-11-05 00:03:11 +0100

    ajout d une activite qui communiquera avec le serveur pour lui dire d envoyer un mail de reset password au mail renseigner par l utilisateur

--- OpenBar/app/src/main/java/com/openbar/frappereauolivier/openbar/Activity/ConnexionActivity.java
+++ OpenBar/app/src/main/java/com/openbar/frappereauolivier/openbar/Activity/ConnexionActivity.java
@@ -42,7 +42,7 @@ public class ConnexionActivity extends Activity {
         //Set the header context
         header.setText(R.string.connexion);
 
-        //Sets action user
+        //Set action user
         buttonLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,6 +51,7 @@ public class ConnexionActivity extends Activity {
         });
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -76,21 +77,42 @@ public class ConnexionActivity extends Activity {
     }
 
     private void onClickButtonLog(View v) {
-        Toast.makeText(getApplicationContext(),"Connexion", Toast.LENGTH_LONG).show();
+        //TODO : Passer les widgets en attributs de cette classe, pour les isntancier qu'une seule fois et les manipuler de partout
+        //TODO : Trouver l'api pour tester si un champ est valide par rapport à une liste de string
+        EditText fieldPseudo = (EditText)this.findViewById(R.id.login_connexion);
+        EditText fieldPassword = (EditText)this.findViewById(R.id.password_connexion);
+        if (fieldPseudo.getText().toString().length() == 0) {
+            fieldPseudo.setError("Pseudo is required");
+        }
+        if (fieldPassword.getText().toString().length() == 0) {
+            fieldPassword.setError("Password is required");
+        }
+
+        if (fieldPseudo.getText().toString().equals("drown")) {
+            if(fieldPassword.getText().toString().equals("drown")) {
+                Toast.makeText(getApplicationContext(),"Connexion", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(),"Bad password", Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Bad pseudo", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void test(View v) {
         switch (v.getId()) {
             case R.id.forget_password :
                 Toast.makeText(getApplicationContext(),"Pass oublié", Toast.LENGTH_LONG).show();
-                //Appeler L'action appropriée
+                //Appeler L'action appropriée (passer par serveur ou web service)
+                //Mais d'abord passer par une activitée nouvelle pour réccupérer le mail.
                 break;
             case R.id.new_account :
                 Toast.makeText(getApplicationContext(),"Form nouveau",Toast.LENGTH_LONG).show();
                 //Appeler L'action appropriée
-                Intent i = new Intent();
-                i.setClass(this,InscriptionActivity.class);
-                startActivity(i);
+                Intent it = new Intent();
+                it.setClass(this,InscriptionActivity.class);
+                startActivity(it);
                 break;
             default:
                 Toast.makeText(getApplicationContext(),"Pb receveur",Toast.LENGTH_LONG).show();
